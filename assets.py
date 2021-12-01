8b3477357d0b1ca61397b840e4cd8d2aa2e36cb4
Mohammad Dori <mr.dori.dev@gmail.com>
2021-12-01 19:02:34 +0330

    Add files via upload

---
+++ assets.py
@@ -0,0 +1,139 @@
+"""Assets of `Dori Calculator` Project
+"""
+
+
+num_btn_stylesheet = """QPushButton{
+    color: white;
+    background-color: rgb(23, 23, 23);
+    font: bold 22px;
+    padding: 6px;
+    border-style: outset;
+    border-width: 2px;
+    border-radius: 10px;
+    border-color: rgb(81, 81, 81);
+}
+
+QPushButton:hover{
+    background-color: rgb(36, 36, 36);
+}
+
+QPushButton:pressed{
+    background-color: black;
+}"""
+
+
+operator_btn_stylesheet = """QPushButton{
+    color: rgb(99, 99, 99);
+    background-color: rgb(246, 244, 244);
+    font: bold 18px;
+    padding: 6px;
+    border-style: outset;
+    border-width: 2px;
+    border-radius: 10px;
+    border-color: rgb(81, 81, 81);
+}
+
+QPushButton:hover{
+    background-color: white;
+}
+
+QPushButton:pressed{
+    background-color: rgb(237, 233, 233);
+}"""
+
+
+equal_btn_stylesheet = """QPushButton{
+    color: black;
+    background-color: rgb(0, 189, 142);
+    font: bold 30px;
+    padding: 6px;
+    border-style: outset;
+    border-width: 2px;
+    border-radius: 10px;
+    border-color: rgb(81, 81, 81);
+}
+
+QPushButton:hover{
+    background-color: rgb(0, 219, 164);
+}
+
+QPushButton:pressed{
+    background-color: rgb(0, 168, 126);
+}"""
+
+
+equal_btn_stylesheet2 = """QPushButton{
+    color: black;
+    background-color: rgb(255, 0, 0);
+    font: bold 30px;
+    padding: 6px;
+    border-style: outset;
+    border-width: 2px;
+    border-radius: 10px;
+    border-color: rgb(81, 81, 81);
+}
+
+QPushButton:hover{
+    background-color: rgb(255, 0, 0);
+}
+
+QPushButton:pressed{
+    background-color: rgb(255, 0, 0);
+}"""
+
+
+delete_btn_stylesheet = """QPushButton{
+    color: white;
+    background-color: rgb(240, 0, 0);
+    font: bold 22px;
+    padding: 6px;
+    border-style: outset;
+    border-width: 2px;
+    border-radius: 10px;
+    border-color: rgb(81, 81, 81);
+}
+
+QPushButton:hover{
+    background-color: rgb(255, 41, 41);
+}
+
+QPushButton:pressed{
+    background-color: rgb(224, 0, 0);
+}"""
+
+
+line_edit_stylesheet = """
+background-color: qlineargradient(x1: 0, y1: 0, x2: 1, y2: 1,stop: 0 #bdc3c7, stop: 1 #000000);
+color: white;
+border-style: outset;
+border-width: 2px;
+border-radius: 10px;
+border-color: rgb(81, 81, 81);
+font: bold 22px;
+padding: 6px;
+"""
+
+
+window_stylesheet = """
+background-color: qlineargradient(x1: 0, y1: 0, x2: 1, y2: 1,stop: 0 #bdc3c7, stop: 0.5 #2c3e50, stop: 1 #000000);
+"""
+
+
+mul_pow_btn_stylesheet = """QPushButton{
+    color: rgb(99, 99, 99);
+    background-color: rgb(246, 244, 244);
+    font: bold 22px;
+    padding: 6px;
+    border-style: outset;
+    border-width: 2px;
+    border-radius: 10px;
+    border-color: rgb(81, 81, 81);
+}
+
+QPushButton:hover{
+    background-color: white;
+}
+
+QPushButton:pressed{
+    background-color: rgb(237, 233, 233);
+}"""
